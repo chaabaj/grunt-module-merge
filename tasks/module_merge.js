6debe71e4b23938b7d766c315a47a0d4ce134ff3
unknown <jalal.chaabane@epitech.eu>
2015-03-10 01:20:35 +0900

    Disable parallel merge it will be implemented later with async-taskjs

--- tasks/module_merge.js
+++ tasks/module_merge.js
@@ -2,64 +2,43 @@
  * Created by CHAABANE on 08/03/2015.
  */
 
-var fork = require('child_process').fork;
-var nbCoreActive = require('os').cpus().length;
 var mkdirp = require('mkdirp');
+var fs = require('fs');
+
+var moduleMerge = function (moduleFiles, dest)
+{
+    moduleFiles.forEach(function (moduleFile)
+    {
+        var content = fs.readFileSync(moduleFile);
+        var moduleDef = JSON.parse(content.toString());
+        var files = moduleDef.files;
+        var outputFile = [dest, moduleDef.name].join('/') + '.js';
+        var modulePath = moduleFile.substring(0, moduleFile.lastIndexOf("/"));
+
+        files.forEach(function (file)
+        {
+            var fullpath = [modulePath, file].join('/');
+
+            fs.appendFileSync(outputFile, fs.readFileSync(fullpath));
+        });
+    });
+    return 0;
+};
+
 
 module.exports = function (grunt)
 {
-    var killAllProcess = function(childs)
-    {
-        childs.forEach(function(child)
-        {
-            child.kill();
-        });
-    };
 
     var moduleMergeInit = function ()
     {
-        var files = this.files.reduce(function(oldValue, file)
+        var files = this.files.reduce(function (oldValue, file)
         {
             return oldValue.concat(file.src);
         }, []);
-        var done = this.async();
-        var childs = [];
-        var nbFilesPerProcess = this.files.length / nbCoreActive;
 
-        if (nbFilesPerProcess < 1)
-        {
-            nbCoreActive = files.length;
-            nbFilesPerProcess = 1;
-        }
         mkdirp.sync(this.files[0].dest);
-        for (var i = 0; i < nbCoreActive; ++i)
-        {
-            var childFiles = files.slice(i * nbFilesPerProcess, (i * nbFilesPerProcess) + nbFilesPerProcess);
+        moduleMerge(files, this.files[0].dest);
 
-            grunt.log.ok('Nb cores : ' + nbCoreActive);
-            var child = fork('src/process-script.js', [this.files[0].dest, childFiles], {
-                cwd : process.cwd()
-            });
-
-            child.on('exit', function ()
-            {
-                nbCoreActive--;
-                if (nbCoreActive === 0)
-                {
-                    done();
-                }
-            });
-
-            child.on('message', function(msg)
-            {
-                if (msg.type === 'error')
-                {
-                    killAllProcess(childs);
-                    grunt.fail.fatal(msg.msg);
-                }
-            });
-            childs.push(child);
-        }
     };
 
     grunt.registerMultiTask('module_merge', 'Merging module files in one files', moduleMergeInit);
